4b7f85e1f19bf61114a80ff6dfea13dd886a9501
davedash <davedash@bcc63047-4c45-0410-b03d-9395e3d64876>
2008-11-29 09:25:56 +0000

    Cleaned up the open id stuff...
    git-svn-id: http://svn.spindrop.us/django/trunk@7 bcc63047-4c45-0410-b03d-9395e3d64876

--- spindrop/django/openid/util.py
+++ spindrop/django/openid/util.py
@@ -175,7 +175,7 @@ def normalDict(request_data):
     values are lists, because in OpenID, each key in the query arg set
     can have at most one value.
     """
-    return dict((k, v[0]) for k, v in request_data.iteritems())
+    return dict((k, v) for k, v in request_data.iteritems())
 
 def normalize_openid(openid):
     """
